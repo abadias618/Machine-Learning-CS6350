4fb123f0c5e69962e0c201008be2e14f4fb5018b
abdias baldiviezo <abadias618@gmail.com>
2022-11-16 00:06:54 -0700

    svm

---
+++ SVM/ssgd_SVM.py
@@ -0,0 +1,40 @@
+import random
+
+class SVM():
+    def __init__(self, X, y, T, bias=0, learning_rate=1e-5):
+        self.X = X
+        self.y = y
+        self.T = T
+        self.weights = self.initialize_weights(len(self.X[0]))
+        self.bias = bias
+        self.learning_rate = learning_rate
+    
+    def initialize_weights(self, length_of_row):
+        weights = []
+        for i in range(length_of_row):
+            weights.append(0)
+        return weights
+    
+    def shuffle_data(self, X, y):
+        merged_X_y = [[X[i] , y[i]] for i in range(len(X))]
+        random.shuffle(merged_X_y)
+        s_X = [merged_X_y[i][0] for i in range(len(merged_X_y))]
+        s_y = [merged_X_y[i][1] for i in range(len(merged_X_y))]
+        return s_X, s_y
+
+    def train(self):
+        # fold bias into weights
+        self.weights = self.weights + [self.bias]
+
+        for _ in range(self.T):
+            X, y = self.shuffle_data(self.X, self.y)
+            for i, row in enumerate(X):
+                # fold bias into vector
+                b_row = row + [self.bias]
+                
+                prediction = self.predict_single(b_row, self.weights)
+                if y[i] != prediction:
+                    #update weights
+                    self.weights = self.update(y[i], b_row, self.weights)
+                 
+        return self.weights
